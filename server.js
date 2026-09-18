4e7ca7ed5c3772effae662057203e44a80304d7c
Madhusudan Mandale <madhusudanmandale387@gmail.com>
2026-09-18 11:30:16 +0530

    Ignore node_modules

---
+++ server.js
@@ -0,0 +1,11 @@
+//sever ko chalana
+//db ko connect karna
+
+import app from "./src/app.js"
+import connectDB from "./src/config/db.js"
+
+connectDB();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
